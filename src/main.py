01ce9e55c01949e9156a276c86f0b853aeb83a65
Absalón <absalon98hg@gmail.com>
2025-01-18 20:58:20 -0600

    r3

--- src/main.py
+++ src/main.py
@@ -1,16 +1,15 @@
 import streamlit as st
-from qreader import QReader
-import cv2
+import qrtools
+
 
 def readQRcodePicture():
-    qreader = QReader()
-    image = cv2.cvtColor(cv2.imread(picture), cv2.COLOR_BGR2RGB)
-    decoded_text = qreader.detect_and_decode(image=image)
-    qtext = decoded_text[0]
+    qr = qrtools.QR()
+    qr.decode("bookmark.png")
+    st.text(qr.data)
 
 enable = st.checkbox("Enable camera")
 picture = st.camera_input("Take a picture", disabled=not enable)
 
 if picture:
-    st.text(readQRcodePicture())
+    readQRcodePicture()
 
