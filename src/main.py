f173824c9d3cee8fc7192fa9f980e80ceeaf6bc5
Absalón <absalon98hg@gmail.com>
2025-01-18 21:23:48 -0600

    r11

--- src/main.py
+++ src/main.py
@@ -1,18 +1,12 @@
 import streamlit as st
-from pyzbar.pyzbar import decode
 from PIL import Image
+import qreader
 
+# Capture image using Streamlit's camera input
 enable = st.checkbox("Enable camera")
-picture = st.camera_input("Take a picture", disabled=not enable)
+image_file = st.camera_input("Take a picture")
 
-def readQRcodePicture():
-    try:
-        qr_code = decode(picture)[0]
-        contenQr = qr_code.data.decode("utf-8")
-        st.text(contenQr)
-    except Exception as err:
-        st.text(str(err))
-
-if picture:
-    readQRcodePicture()
-
+if image_file is not None:
+    image = Image.open(image_file)
+    qr_data = qreader.QReader().detect_and_decode(image)
+    st.write("QR Code Data:", qr_data)
