f47e5e59e2b11837eb3e70217dd1c29b0e23eddb
Absalón <absalon98hg@gmail.com>
2025-01-18 22:41:01 -0600

    r27

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ import pandas as pd
 #Read google sheet
 conn = st.connection("gsheets", type=GSheetsConnection)
 existing_data = conn.read(worksheet="DatosForms")
+df = pd.DataFrame(existing_data)
 name_to_check = "Hola"
 
 # Picture
@@ -45,9 +46,6 @@ if uploaded_file is not None:
         st.write("No QR code found in the image.")
 
 if st.button("Validate and Update"):
-    st.cache_data.clear()
-    existing_data = conn.read(worksheet="DatosForms")
-    df = pd.DataFrame(existing_data)
     updated = validate_and_update_name(df, name_to_check)
     if updated:
         # Write the updated DataFrame back to the Google Sheet
