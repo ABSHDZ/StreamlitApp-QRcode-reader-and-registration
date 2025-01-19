a889ca0fcb3b22b0949446d0e87d411b0a08e6b1
Absalón <absalon98hg@gmail.com>
2025-01-18 22:49:02 -0600

    r30

--- src/main.py
+++ src/main.py
@@ -11,22 +11,9 @@ import pandas as pd
 conn = st.connection("gsheets", type=GSheetsConnection)
 existing_data = conn.read(worksheet="DatosForms")
 df = pd.DataFrame(existing_data)
-name_to_check = "Hola"
 
 # Picture
 uploaded_file = st.camera_input("Take a picture")
-
-def validate_and_update_name(df, name_to_check):
-    # Check if the name exists in the NombreCompleto column
-    if name_to_check in df['NombreCompleto'].values:
-        # Find the index of the row where the name matches
-        idx = df[df['NombreCompleto'] == name_to_check].index[0]
-
-        # Check if the Payment column is empty
-        if df.at[idx, 'Payment'] == "":
-            df.at[idx, 'Payment'] = "Paid"  # Update the value
-            return True  # Indicate that an update was made
-    return False  # No update needed
 
 if uploaded_file is not None:
     # Convert the uploaded image to a format suitable for OpenCV
@@ -46,11 +33,4 @@ if uploaded_file is not None:
         st.write("No QR code found in the image.")
 
 if st.button("Validate and Update"):
-    updated = validate_and_update_name(df, name_to_check)
-    if updated:
-        # Write the updated DataFrame back to the Google Sheet
-        conn.write(df, worksheet="DatosForms")
-        st.success(f"Payment status updated for {name_to_check}.")
-    else:
-        st.warning(f"No update needed for {name_to_check} (name not found or already paid).")
-
+    st.dataframe(df)
