aa8c8b6fdfd66974e3bbc6779a5b599b5a570864
Absalón <absalon98hg@gmail.com>
2025-01-18 21:04:34 -0600

    r4

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
 import streamlit as st
-import qrtools
-
-
-def readQRcodePicture():
-    qr = qrtools.QR()
-    qr.decode("bookmark.png")
-    st.text(qr.data)
+import pyzbar
 
 enable = st.checkbox("Enable camera")
 picture = st.camera_input("Take a picture", disabled=not enable)
+
+def readQRcodePicture():
+    image = cv2.imread(picture)
+    decoded = pyzbar.decode(image)
+    st.text(decoded.data)
 
 if picture:
     readQRcodePicture()
